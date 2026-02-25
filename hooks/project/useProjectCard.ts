66de972855bd404f7c7587ca554b3d5c706117ce
AlvinWesley <wesealv@gmail.com>
2026-02-25 07:21:08 -0600

    add a realtime channel update for cards

--- hooks/project/useProjectCard.ts
+++ hooks/project/useProjectCard.ts
@@ -1,8 +1,10 @@
 import { OrgProjectCard } from "@/lib/card-utils"
-import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/lib/supabase/supabase";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export const useProjectCard=()=>{
-
+const queryClient=useQueryClient();
     const getProjectCard=async()=>{
         try{
             const res= await fetch("/api/projects/cards",{
@@ -26,6 +28,15 @@ export const useProjectCard=()=>{
         queryKey:['OrgProjectCards'],
         queryFn:()=>getProjectCard(),
     })
+    useEffect(()=>{
+        const channel =supabase.channel('org_project_channel').on('postgres_changes',{event:'*',schema:'public',table:'OrganizationProject'},()=>{
+            queryClient.invalidateQueries({queryKey:['OrgProjectCards']})
+        }).subscribe()
+        return()=>{
+            supabase.removeChannel(channel)
+        }
+    },[queryClient])
+
 return{
     filteredProjects:projectData.data??[],
     loading:projectData.isLoading,
